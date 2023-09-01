4e175cd91a26a94c24a478d1414d4e468177a3ac
Lin Fan-Chiang <yusuke050@gmail.com>
2023-09-01 23:11:52 +0800

    Update 2705 Compact Object.js

--- 2705 Compact Object.js
+++ 2705 Compact Object.js
@@ -16,3 +16,23 @@ var compactObject = function(obj) {
     }
     return obj;
 };
+
+
+/**
+ * v2 accepted, runtime: 76 ms (91.89%), memory: 51.29 MB (93.86%)
+ */
+var compactObject = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.filter(Boolean).map(compactObject);
+    } else if (typeof obj === 'object' && obj !== null) {
+        for (const key in obj) {
+            if (!obj[key]) {
+                delete obj[key];
+            } else {
+                obj[key] = compactObject(obj[key]);
+            }
+        }
+        return obj;
+    }
+    return obj;
+};
