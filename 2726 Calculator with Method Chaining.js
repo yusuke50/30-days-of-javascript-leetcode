0f600421633aa46fa68b4b14ef5af3b40625c2f3
Lin Fan-Chiang <yusuke050@gmail.com>
2023-09-02 19:44:07 +0800

    Create 2726 Calculator with Method Chaining.js

---
+++ 2726 Calculator with Method Chaining.js
@@ -0,0 +1,135 @@
+/**
+ * v1 accepted, runtime: 54 ms (50.49%), memory: 42.1 MB (13.92%)
+ */
+class Calculator {
+  /**
+   * @param {number} value
+   */
+  constructor(value) {
+    this.result = value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {
+    if (value === 0) {
+      throw new Error('Division by zero is not allowed');
+    }
+    this.result /= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {
+    this.result **= value;
+    return this;
+  }
+
+  /**
+   * @return {number}
+   */
+  getResult() {
+    return this.result;
+  }
+}
+
+/**
+ * v2 accepted, runtime: 48 ms (79.5%), memory: 41.9 MB (42.55%)
+ */
+class Calculator {
+  /**
+   * @param {number} value
+   */
+  constructor(value) {
+    this.result = value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  divide(value) {
+    if (value === 0) {
+      throw new Error('Division by zero is not allowed');
+    }
+    this.result /= value;
+    return this;
+  }
+
+  /**
+   * @param {number} value
+   * @return {Calculator}
+   */
+  power(value) {
+    this.result = Math.pow(this.result, value);
+    return this;
+  }
+
+  /**
+   * @return {number}
+   */
+  getResult() {
+    return this.result;
+  }
+}
